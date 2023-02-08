5125ac746fc162d5a6d28ccd60618095d05d4b24
toaryappwrk <tory@appwrk.co>
2023-02-08 11:28:49 +0530

    slider functionality changes

--- css/custom.js
+++ css/custom.js
@@ -1,12 +1,14 @@
-if(document.getElementById('next')){
-  document.getElementById('next').onclick = function(){
-    const widthItem = document.querySelector('.item').offsetWidth;
-    document.getElementById('formList').scrollLeft += widthItem;
+setInterval(function(){
+  if(document.getElementById('next')){
+    document.getElementById('next').onclick = function(){
+      const widthItem = document.querySelector('.item').offsetWidth;
+      document.getElementById('formList').scrollLeft += widthItem;
+    }
   }
-}
-if(document.getElementById('prev')){
-  document.getElementById('prev').onclick = function(){
-    const widthItem = document.querySelector('.item').offsetWidth;
-    document.getElementById('formList').scrollLeft -= widthItem;
+  if(document.getElementById('prev')){
+    document.getElementById('prev').onclick = function(){
+      const widthItem = document.querySelector('.item').offsetWidth;
+      document.getElementById('formList').scrollLeft -= widthItem;
+    }
   }
-}
+},4000);
